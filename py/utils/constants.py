6c4b7508e95415e17eec96823a69cf61520f86cc
TBG______ <laarmann@ylab.es>
2025-08-27 04:22:13 +0200

    Update constants.py

--- py/utils/constants.py
+++ py/utils/constants.py
@@ -1,9 +1,7 @@
 # # from ComfyUI/custom_nodes/TBG_upscaler/py/vendor/ComfyUI_MaraScott_Nodes check licence in this folder
 import re
 
-#APIURL ='https://api.ylab.es/'
-#APIURL ='http://localhost:8080/'
-APIURL ='https://dev---tbg-api-oom2umomra-uc.a.run.app/'
+APIURL ='https://api.ylab.es/'
 
 NAMESPACE='TBG'
 ICON=''
